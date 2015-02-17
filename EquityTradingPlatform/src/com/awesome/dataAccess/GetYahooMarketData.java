ecea7a82903853d6ce3e40d7b8b650b39d395b4a
jwalmsley01 <jwalmsley01@qub.ac.uk>
2015-02-17 13:09:03 +0000

    Fixed logic and format with GetYahooMarketData

--- EquityTradingPlatform/src/com/awesome/dataAccess/GetYahooMarketData.java
+++ EquityTradingPlatform/src/com/awesome/dataAccess/GetYahooMarketData.java
@@ -1,89 +1,104 @@
 package com.awesome.dataAccess;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
- 
+
 // Get quote data for a list of symbols and returns it in a Map
 
-public class GetYahooMarketData 
-{
-    // Quote Data class
-    public class QuoteData
-    {
-        public float askPrice;
-        public float bidPrice;
-        public int   askSize;
-        public int   bidSize;
-    }
+public class GetYahooMarketData {
+	// Quote Data class
+	public class QuoteData {
+		public float askPrice;
+		public float bidPrice;
+		public int askSize;
+		public int bidSize;
 
-    public Map<String, QuoteData> getQuote(String[] stocks) throws Exception
-    {
-        // Build the URL  
-        StringBuilder url = 
-            new StringBuilder("http://download.finance.yahoo.com/d/quotes.csv?s=");
-        for (String s : stocks)
-            url.append(s + ",");
-        url.deleteCharAt(url.length()-1);
-        // Properties is for bid and ask
-        url.append("&f=a0a5b0b6&e=.csv");
- 
-        // Get the csv lines from Yahoo
-        List<String> csv = getCsv(url.toString());
+		public float closePrice;
+		public int volume;
+	}
 
-        // Build response
-        Map<String, QuoteData> quotes = new HashMap<String, QuoteData>();
-        int i = 0;
-        for (String line : csv)
-        {
-            // Parse csv lines
-            String[] fields = line.split(",", -1);
+	public Map<String, QuoteData> getQuote(String[] stocks) throws Exception {
+		// Build the URL
+		StringBuilder url = new StringBuilder(
+				"http://download.finance.yahoo.com/d/quotes.csv?s=");
+		for (String s : stocks)
+			url.append(s + ",");
+		url.deleteCharAt(url.length() - 1);
+		// Properties is for bid and ask
+		url.append("&f=a0a5b0b6p0v0&e=.csv");
 
-            // Should be 4 values if not discard line
-            if (fields.length == 4)
-            {
-                QuoteData quote = new QuoteData();
-                try
-                {
-                    quote.askPrice = Float.parseFloat(fields[0]);
-                    quote.askSize = Integer.parseInt(fields[1]);
-                    quote.bidPrice = Float.parseFloat(fields[2]);
-                    quote.bidSize = Integer.parseInt(fields[3]);
-                    // Insert quote with stock as key
-                    quotes.put(stocks[i], quote);
-                } 
-                catch (NumberFormatException e) {}
-                
-            }
-            i++;
-        }
-        return quotes;
-    }
+		// Get the csv lines from Yahoo
+		List<String> csv = getCsv(url.toString());
 
-    // Returns list of csv from Yahoo
-    private List<String> getCsv(String url) throws Exception
-    {
-        List<String> response = new ArrayList<String>();
-        URL obj = new URL(url);
+		// Build response
+		Map<String, QuoteData> quotes = new HashMap<String, QuoteData>();
+		int i = 0;
+		for (String line : csv) {
+			// Parse csv lines
+			String[] fields = line.split(",", -1);
 
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // This is a GET request
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        int responseCode = con.getResponseCode();
-        // If the response code is not OK then return empty line
-        if (responseCode != 200)
-            return response; 
+			// Should be 4 values if not discard line
+			// I think there is an error with this if
+			if (fields.length == 6 || fields.length == 8) {
+				QuoteData quote = new QuoteData();
+				try {
+					if (fields.length == 6) {
 
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
+						quote.askPrice = Float.parseFloat(fields[0]);
+						quote.askSize = Integer.parseInt(fields[1]);
+						quote.bidPrice = Float.parseFloat(fields[2]);
+						quote.bidSize = Integer.parseInt(fields[3]);
+						quote.closePrice = Float.parseFloat(fields[4]);
+						quote.volume = Integer.parseInt(fields[5]);
 
-        while ((inputLine = in.readLine()) != null)
-            response.add(inputLine);
+					} else {
+						quote.askPrice = Float.parseFloat(fields[0]);
+						quote.bidPrice = Float.parseFloat(fields[3]);
+						quote.closePrice = Float.parseFloat(fields[6]);
+						quote.volume = Integer.parseInt(fields[7]);
 
-        in.close();
-        return response;
-    }
+						quote.askSize = (1000 * Integer.parseInt(fields[1]) + Integer
+								.parseInt(fields[2]));
+						quote.bidSize = (1000 * Integer.parseInt(fields[4]) + Integer
+								.parseInt(fields[5]));
+
+					}
+					// Insert quote with stock as key
+					quotes.put(stocks[i], quote);
+				} catch (NumberFormatException e) {
+				}
+
+			}
+			i++;
+		}
+		return quotes;
+	}
+
+	// Returns list of csv from Yahoo
+	private List<String> getCsv(String url) throws Exception {
+		List<String> response = new ArrayList<String>();
+		URL obj = new URL(url);
+
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		// This is a GET request
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		int responseCode = con.getResponseCode();
+		// If the response code is not OK then return empty line
+		if (responseCode != 200)
+			return response;
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				con.getInputStream()));
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null)
+			response.add(inputLine);
+
+		in.close();
+		return response;
+	}
 }
--- EquityTradingPlatform/src/com/awesome/dataAccess/TestYahoo.java
+++ EquityTradingPlatform/src/com/awesome/dataAccess/TestYahoo.java
@@ -8,22 +8,20 @@ public class TestYahoo
  public static void main(String[] args) throws Exception
  {
      GetYahooMarketData yahooReader = new GetYahooMarketData();
-     String [] quotes = {"YHOO", "AAPL"};
+     String [] quotes = {"IBMD"};
      while (true)
      {
          // Get Quotes
-         Map<String, GetYahooMarketData.QuoteData> data = 
-                                                 yahooReader.getQuote(quotes);
+         Map<String, GetYahooMarketData.QuoteData> data = yahooReader.getQuote(quotes);
+         
          for (Map.Entry<String, GetYahooMarketData.QuoteData> entry : 
               data.entrySet())
          {
              GetYahooMarketData.QuoteData quote = entry.getValue();
-             System.out.printf ("%s [%dx%.2f] x [%.2fx%d]\n",
+             System.out.printf ("%s [%.2f] x [%d]\n",
                                 entry.getKey(),
-                                quote.bidSize,
-                                quote.bidPrice,
-                                quote.askPrice,
-                                quote.askSize);
+                                quote.closePrice,
+                                quote.volume);
          }
      }
  }
